d3400f6fb906c7c106b4c63343acb60c570f45d1
Gerry Yang <gerry04y@gmail.com>
2025-04-02 19:38:07 +0100

    what we do

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import HeroSection from '@/components/home/HeroSection';
+import WhatWeDoSection from '@/components/home/WhatWeDoSection';
 import OurApproachSection from '@/components/home/OurApproachSection';
 import ContactSection from '@/components/home/ContactSection';
 import AITransformation from '@/components/home/AITransformation';
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <>
       <HeroSection />
+      <WhatWeDoSection />
       <AITransformation />
       <OurApproachSection />
       <ContactSection />
